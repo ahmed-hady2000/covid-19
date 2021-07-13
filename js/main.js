49d0fb1086881928d58436ce6ce681e3d6efdf5e
ahmed-hady2000 <82987366+ahmed-hady2000@users.noreply.github.com>
2021-07-13 14:12:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let them = () => {
         nav.style.background ="balck";
         lists.style.color="#00000080";
         faqs.style.color="#262930";
-        aaa.style.background="#ebebebc2"; 
+        aaa.style.background="#ebebeb"; 
 
 
     } else {
@@ -32,72 +32,6 @@ let them = () => {
 }
 btn.addEventListener('click',them);
 
-$('.color-faq').click(function() {
-    $('.color-faq').removeClass('active');
-    $(this).addClass('active');
-});
-$('.faq-text .item').first().show();
-$('.color-faq').click(function(){
-    let indexofColorfaq = $(this).index();
-    $('.faq-text .item').hide();
-    $('.faq-text .item').eq(indexofColorfaq).show();
-});
-
-
-
-
-$('.color-faq').click(function() {
-    $('.color-faq').removeClass('active');
-    $(this).addClass('active');
-});
-$('.faq-text .item').first().show();
-$('.color-faq').click(function(){
-    let indexofColorfaq = $(this).index();
-    $('.faq-text .item').hide();
-    $('.faq-text .item').eq(indexofColorfaq).show();
-});
-
-
-
-
-// faq test-1
-
-$('.click1').click(function(){
-    $('.faq-text ul li.click1').toggleClass('style-li');
-    $('.faq-text ul li.click1 .icon-minus').toggle();
-    $('.faq-text ul li.click1 .icon-plus').toggle();
-    $('.faq-text ul li.click1 + .ul-text').slideToggle();
-});
-$('.click2').click(function(){
-    $('.faq-text ul li.click2').toggleClass('style-li');
-    $('.faq-text ul li.click2 .icon-minus').toggle();
-    $('.faq-text ul li.click2 .icon-plus').toggle();
-    $('.faq-text ul li.click2 + .ul-text').slideToggle();
-});    
-$('.click3').click(function(){
-    $('.faq-text ul li.click3').toggleClass('style-li');
-    $('.faq-text ul li.click3 .icon-minus').toggle();
-    $('.faq-text ul li.click3 .icon-plus').toggle();
-    $('.faq-text ul li.click3 + .ul-text').slideToggle();
-});
-$('.click4').click(function(){
-    $('.faq-text ul li.click4').toggleClass('style-li');
-    $('.faq-text ul li.click4 .icon-minus').toggle();
-    $('.faq-text ul li.click4 .icon-plus').toggle();
-    $('.faq-text ul li.click4 + .ul-text').slideToggle();
-});
-$('.click5').click(function(){
-    $('.faq-text ul li.click5').toggleClass('style-li');
-    $('.faq-text ul li.click5 .icon-minus').toggle();
-    $('.faq-text ul li.click5 .icon-plus').toggle();
-    $('.faq-text ul li.click5 + .ul-text').slideToggle();
-});
-$('.click6').click(function(){
-    $('.faq-text ul li.click6').toggleClass('style-li');
-    $('.faq-text ul li.click6 .icon-minus').toggle();
-    $('.faq-text ul li.click6 .icon-plus').toggle();
-    $('.faq-text ul li.click6 + .ul-text').slideToggle();
-});
 
 
 
